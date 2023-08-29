1e6d8ce8f6ea74b5ea07bbb57624552397db2b23
Piotr Kuraś <piotr.kuras.1997@gmail.com>
2023-08-29 08:41:19 +0200

    Update FetchFromAPI.js

--- src/utils/FetchFromAPI.js
+++ src/utils/FetchFromAPI.js
@@ -1,20 +1,16 @@
 import axios from "axios"
 
-const BASE_URL = 'https://newsdata2.p.rapidapi.com/news'
+const BASE_URL = 'https://newsdata2.p.rapidapi.com/news?apikey=pub_28448ba4ccb6fd552f8c2160e62e7e760e462'
 
 const options = {
     params: {
         language: 'en'
-    },
-    headers: {
-        'X-RapidAPI-Key': '0a13eea310msh05b908c0d48042cp11462cjsn1d4dc2b46242',
-        'X-RapidAPI-Host': 'newsdata2.p.rapidapi.com'
     }
 }
 
 
 
 export const fetchFromAPI = async(url) => {
-    const { data } = await axios.get(`${BASE_URL}?${url}`, options)
+    const { data } = await axios.get(`${BASE_URL}${url}`, options)
     return data
 }
